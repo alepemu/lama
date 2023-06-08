c8041d5a453db21f6b81445bd8517657cc3d0a10
alejandrop <geo.al3x@gmail.com>
2023-06-08 10:46:37 +0200

    feat: login and signup data ready to be sent

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,16 +1,47 @@
+import { useState } from 'react';
 import './LogIn.css';
 import { Link } from 'react-router-dom';
 
 function LogIn() {
+  const [userLogin, setUserLogin] = useState({ email: '', pw: '' });
+
+  function handleInput(e) {
+    const { name, value } = e.target;
+    setUserLogin({ ...userLogin, [name]: value });
+  }
+
+  function logInUser(e) {
+    e.preventDefault();
+    const userLoginValues = {
+      email: userLogin.email,
+      password: userLogin.pw,
+    };
+    setUserLogin({ email: '', pw: '' });
+  }
+
   return (
     <div className="LogIn">
       <h3>Log in</h3>
 
-      <form>
-        <label>Email:</label>
-        <input type="email" placeholder="Enter your email"></input>
-        <label>Password:</label>
-        <input type="password" placeholder="Enter your password"></input>
+      <form onSubmit={logInUser}>
+        <label htmlFor="form-email">Email:</label>
+        <input
+          type="email"
+          id="form-email"
+          name="email"
+          value={userLogin.email}
+          onChange={handleInput}
+          placeholder="Enter your email"
+        ></input>
+        <label htmlFor="form-pw">Password:</label>
+        <input
+          type="password"
+          id="form-pw"
+          name="pw"
+          value={userLogin.pw}
+          onChange={handleInput}
+          placeholder="Enter your password"
+        ></input>
         <button type="submit">Log In</button>
         <div className="form-footer">
           <p>If you still don't have an account,</p>
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,18 +1,57 @@
+import { useState } from 'react';
 import './SignUp.css';
 import { Link } from 'react-router-dom';
 
 function SignUp() {
+  const [userData, setUserData] = useState({ name: '', email: '', pw: '' });
+
+  function handleInput(e) {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  }
+
+  function createNewUser(e) {
+    e.preventDefault();
+    const userDataValues = {
+      name: userData.name,
+      email: userData.email,
+      password: userData.pw,
+    };
+    setUserData({ name: '', email: '', pw: '' });
+  }
+
   return (
     <div className="SignUp">
       <h3>New user</h3>
 
-      <form>
-        <label for="name">Name</label>
-        <input type="text" name="name" placeholder="Enter user name"></input>
-        <label>Email:</label>
-        <input type="email" placeholder="Enter valid email"></input>
-        <label>Password:</label>
-        <input type="password" placeholder="Enter password"></input>
+      <form onSubmit={createNewUser}>
+        <label htmlFor="form-name">Name</label>
+        <input
+          type="text"
+          id="form-name"
+          name="name"
+          value={userData.name}
+          onChange={handleInput}
+          placeholder="Enter user name"
+        ></input>
+        <label htmlFor="form-email">Email:</label>
+        <input
+          type="email"
+          id="form-email"
+          name="email"
+          value={userData.email}
+          onChange={handleInput}
+          placeholder="Enter valid email"
+        ></input>
+        <label htmlFor="form-pw">Password:</label>
+        <input
+          type="password"
+          id="form-pw"
+          name="pw"
+          value={userData.pw}
+          onChange={handleInput}
+          placeholder="Enter password"
+        ></input>
         <button type="submit">Register</button>
         <div className="form-footer">
           <p>If you already have an account,</p>
