860b6c75c271bed8e00b793914d02fd10cf715b8
alejandrop <geo.al3x@gmail.com>
2023-06-08 10:10:30 +0200

    feat: lohin and signup basic forms

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,13 +1,23 @@
 import './LogIn.css';
 import { Link } from 'react-router-dom';
 
-
 function LogIn() {
   return (
     <div className="LogIn">
-      <h1>This is LogIn</h1>
-      <h1>This is LogIn</h1>
-      <Link to="/signup">Sign Up</Link>
+      <h3>Log in</h3>
+
+      <form>
+        <label>Email:</label>
+        <input type="email" placeholder="Enter your email"></input>
+        <label>Password:</label>
+        <input type="password" placeholder="Enter your password"></input>
+        <button type="submit">Log In</button>
+        <div className="form-footer">
+          <p>If you still don't have an account,</p>
+          <Link to="/signup">register</Link>
+          <p>now.</p>
+        </div>
+      </form>
     </div>
   );
 }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -4,10 +4,22 @@ import { Link } from 'react-router-dom';
 function SignUp() {
   return (
     <div className="SignUp">
-      <h1>This is SignUp</h1>
-      <h1>This is SignUp</h1>
-      <Link to="/login">Log In</Link>
+      <h3>New user</h3>
 
+      <form>
+        <label for="name">Name</label>
+        <input type="text" name="name" placeholder="Enter user name"></input>
+        <label>Email:</label>
+        <input type="email" placeholder="Enter valid email"></input>
+        <label>Password:</label>
+        <input type="password" placeholder="Enter password"></input>
+        <button type="submit">Register</button>
+        <div className="form-footer">
+          <p>If you already have an account,</p>
+          <Link to="/login">Log In</Link>
+          <p>now.</p>
+        </div>
+      </form>
     </div>
   );
 }
