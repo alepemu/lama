74d82ca15f88f873535502451eb9fc7868d1e632
alejandrop <geo.al3x@gmail.com>
2023-06-08 17:11:56 +0200

    feat: basic input text for new category working

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,6 +7,8 @@ import './Dashboard.css';
 function Dashboard() {
   const [user, setUser] = useState({});
   const [userCat, setUserCat] = useState([]);
+  const [newCatTitle, setNewCat] = useState('');
+
   let currentUserId = '6480a98535fbc7221e4f2eb2';
 
   useEffect(() => {
@@ -23,12 +25,19 @@ function Dashboard() {
     <Category key={catId} catId={catId} setUserCat={setUserCat} />
   ));
 
-  function newCategory() {
-    const currentUser = user._id;
-    const content = { userId: currentUser, content: { name: 'newCatFromWeb1', color: 'default' } };
+  function newCategory(e) {
+    e.preventDefault();
+    if (newCatTitle === '') {
+      alert('Give a name!');
+      return;
+    }
+    const content = { userId: user._id, content: { name: newCatTitle, color: 'default' } };
     newCat(content)
-      .then((cat) => setUserCat([...userCat, cat]))
+      .then((cat) => {
+        setUserCat([...userCat, cat._id]);
+      })
       .catch((error) => console.log(error));
+    setNewCat('');
   }
 
   return (
@@ -38,9 +47,18 @@ function Dashboard() {
         <h3>Notification Settings</h3>
       </div>
       <div id="dashboard-pool">{catList}</div>
-      <button className="btn-new-cat" onClick={newCategory}>
-        Add New Category
-      </button>
+      <form onSubmit={newCategory}>
+        <label htmlFor="input-new-cat">New Category</label>
+        <input
+          type="text"
+          id="input-new-cat"
+          name="new-cat-name"
+          value={newCatTitle}
+          onChange={(e) => setNewCat(e.target.value)}
+          placeholder="Enter a new category name"
+        ></input>
+        <button className="btn-new-cat">Add New Category</button>
+      </form>
     </div>
   );
 }
--- client/src/services/ApiCategory.js
+++ client/src/services/ApiCategory.js
@@ -1,4 +1,5 @@
 async function loadCat(catId) {
+  console.log('catId en api', typeof catId, catId);
   const response = await fetch('http://localhost:3100/category/' + catId);
   const category = await response.json();
   return category;
