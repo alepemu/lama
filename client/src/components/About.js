e9f9455591381c9ed572c7ed275e433027811cb5
alejandrop <geo.al3x@gmail.com>
2023-06-13 12:13:38 +0200

    feat: about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,16 @@
 import './About.css';
+import lama_hi_img from './../img/img-lama-hi.jpg';
 
 function About() {
   return (
     <div className="About">
       <div id="about-container">
-        <h3>want to know more about llamas?</h3>
-        <ol>
+        <img id="about-llama-pic" src={lama_hi_img} alter="Pretty llama"></img>
+        <div id="about-title">
+          <h3>Some facts about llamas</h3>
+          <p>...hover below to discover!</p>
+        </div>
+        <ul>
           <li>
             Llamas are known for their spitting abilities, but they don't actually spit out of
             anger. They usually spit to communicate with each other or to express their annoyance.
@@ -21,21 +26,6 @@ function About() {
             conquering the summit!
           </li>
           <li>
-            Llamas are social animals and often live in herds. They have a strong sense of community
-            and will even hum to communicate with each other. So, you could say they're the original
-            hum-mates!
-          </li>
-          <li>
-            Llamas have a keen sense of curiosity and are known to investigate objects by giving
-            them a gentle nudge with their nose. If you leave something interesting lying around,
-            don't be surprised if a llama comes up to inspect it!
-          </li>
-          <li>
-            Llamas are environmentally friendly lawnmowers! Some people use llamas to graze on their
-            lawns instead of using gas-powered mowers. It's a win-win situation: the llamas get a
-            tasty meal, and the lawn gets a trim.
-          </li>
-          <li>
             Llamas have a reputation for being quirky and can display amusing behaviors. They often
             make funny facial expressions and have been known to photobomb unsuspecting tourists
             taking selfies. Watch out for those photogenic llamas!
@@ -45,11 +35,7 @@ function About() {
             give them a gentle nuzzle or a "kiss" by pressing their lips against the person's cheek.
             Who wouldn't want a smooch from a llama?
           </li>
-        </ol>
-        <p>
-          Remember, llamas are fascinating creatures with a great sense of humor. They never fail to
-          bring a smile to people's faces!
-        </p>
+        </ul>
       </div>
     </div>
   );
