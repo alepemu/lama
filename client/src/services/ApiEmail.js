5312fd2dc897d75e864013cb2a3cf9be15b48b4a
alepemu <geo.al3x@gmail.com>
2023-07-11 21:12:50 +0200

    fix: fly io url

--- client/src/services/ApiEmail.js
+++ client/src/services/ApiEmail.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://lama.fly.dev';
-const BASE_URL = 'http://localhost:3100';
+const BASE_URL = 'https://lama.fly.dev';
+// const BASE_URL = 'http://localhost:3100';
 
 async function sendEmail(userId) {
   await fetch(BASE_URL + '/email/' + userId);
