44a654e269f201a60937cca26efb9eb4e25b2fed
alepemu <geo.al3x@gmail.com>
2023-08-15 13:13:58 +0200

    fix: model index uri

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-const Koa = require('koa');
-const cors = require('@koa/cors');
-const bodyParser = require('koa-bodyparser');
-// const session = require('koa-session');
-const jwt = require('koa-jwt');
+const Koa = require("koa");
+const cors = require("@koa/cors");
+const bodyParser = require("koa-bodyparser");
+const jwt = require("koa-jwt");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const router = require('./router.js');
+const router = require("./router.js");
 const app = new Koa();
 
 const PORT = process.env.SERVER_PORT || 3100;
@@ -17,26 +16,14 @@ const CORSCONFIG = {
   credentials: true,
 };
 
-// const CONFIG = {
-//   key: 'koa.sess',
-//   maxAge: 86400000,
-//   httpOnly: false,
-//   signed: false, /** (boolean) signed or not (default true) */
-//   secure: false, /** (boolean) secure cookie*/
-//   sameSite: null, /** (string) session cookie sameSite options (default null, don't set it) */
-// };
+require("./services/service.dispatcher.js");
 
-require('./services/service.dispatcher.js');
-
-// app.keys = ['not a real secret'];
-
-// app.use(session(CONFIG, app))
-app.use(jwt({ secret: 'shared-secret', passthrough: true }));
+app.use(jwt({ secret: "shared-secret", passthrough: true }));
 app.use(cors(CORSCONFIG));
 app.use(bodyParser());
 
 app.use(router.routes());
 
 app.listen(PORT, () => {
-  console.log('Server running on port', PORT);
+  console.log("Server running on port", PORT);
 });
--- server/models/index.js
+++ server/models/index.js
@@ -2,20 +2,19 @@
 
 const mongoose = require("mongoose");
 
-// const { FLY_IO } = process.env;
+const { FL0 } = process.env;
 
-// const uri = FLY_IO ?
-//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@lama-web.sniwull.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority` :
-//     `mongodb://127.0.0.1:${process.env.DB_PORT || '27017'}/${process.env.DB_NAME || 'lama_beta'}`;
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@lama-web.sniwull.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = FL0
+  ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@lama-web.sniwull.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  : `mongodb://127.0.0.1:${process.env.DB_PORT || "27017"}/${
+      process.env.DB_NAME || "lama_beta"
+    }`;
 
 async function main() {
   try {
     await mongoose.connect(uri);
     console.log(
-      //   "Connected to the MongoDB " + (FLY_IO ? "Atlas" : "local") + " database"
-      "Connected to the MongoDB Atlas database"
+      "Connected to the MongoDB " + (FL0 ? "Atlas" : "local") + " database"
     );
   } catch (err) {
     console.log("Something went wrong when connecting to the database : ", err);
